b24c1ecdf33529fd0e1f4632a50c5dcc9e03f261
MorganeLara <morganelara.design@gmail.com>
2022-11-06 17:21:35 +0100

    added most styling (css)

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  if (props.phonetic) {
+  const PhoneticsAudio = props.phonetic.audio;
+
+  function playAudio() {
+    const audio = new Audio(PhoneticsAudio);
+    audio.play();
+  }
+  if (props.phonetic.audio) {
     return (
-      <div className="Phonetic">
-        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-          Listen
-        </a>
-        <br />
-        {props.phonetic.text}
-      </div>
+      <span className="Phonetic phonetic-block">
+        <span>
+          <FontAwesomeIcon icon={faPlay} onClick={playAudio} />
+        </span>
+        <span className="text">{props.phonetic.text}</span>
+      </span>
     );
+  } else {
+    return null;
   }
 }
